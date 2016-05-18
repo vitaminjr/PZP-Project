0e07499e4ac2ba0b0726a83c4daed77be041f978
vitaminjr <vitaminjr4@gmail.com>
2016-05-18 22:05:02 +0300

    Commit

---
+++ src/Interface/InformationCompInterface.java
@@ -0,0 +1,24 @@
+package Interface;
+
+/**
+ * Created by vitaminjr on 18.05.16.
+ */
+public interface InformationCompInterface {
+    String getTypeProcessor();
+    String getLoadingProcessor();
+    String getSocketProcessor();
+    String getBitProcessor();
+    String getLevCacheProcessor();
+    String getSizeMemory();
+    String getBusyMemory();
+    String getFreeMemory();
+    String getLoadingMemory();
+    String getTypeMempry();
+    String getSpeedMemory();
+    String getTypeGraphic();
+    String getCapacityGraphic();
+    String getTypeTiresGraphic();
+    String getWidthTiresGraphic();
+    String getTypeDevices();
+    String getCharacDevices();
+}
--- src/mainForm/Controller.java
+++ src/mainForm/Controller.java
@@ -71,6 +71,7 @@ public class Controller {
                 monitooring.getDevises();
             }
         });
+        listViewProcessor.setAccessibleText("аплвалдп");
 
 
     }
